57dcfa67665aaa3acc80f148b6dfbdf07cf407bc
junesutan <juneowns@gmail.com>
2025-10-07 11:56:11 +0800

    dropdown and input box work, now to fetch rates  from API

--- currency-converter/src/components/Converter.jsx
+++ currency-converter/src/components/Converter.jsx
@@ -1,26 +1,46 @@
 import React from "react";
 import { useState } from "react";
-
-const dummyExchangeRate = 0.7742;
+const DUMMY_EXCHANGE_RATE = 0.7742;
 
 const Converter = () => {
   const [leftCurrency, setLeftCurrency] = useState("sgd");
   const [rightCurrency, setRightCurrency] = useState("usd");
-
   const [leftValue, setLeftValue] = useState("1.00");
-  const [rightValue, setRightValue] = useState("1.00");
+  const [rightValue, setRightValue] = useState("");
 
   //   const handleChange = (event) => {
   //     setBaseValue(event.target.value);
   //   };
 
-  //WRITING A FUNCTION TO HANDLE HOW TYPING ON THE LEFT UPDATES THE TEXTBOX ON THE RIGHT
+  //FUNCTION TO HANDLE HOW TYPING ON THE LEFT UPDATES THE TEXTBOX ON THE RIGHT
   const onLeftChange = (e) => {
-    const v = e.target.value;
+    const v = e.target.value; // let v be what the user typed
     setLeftValue(v);
-    if (rate && v !== "") {
-      const value = parseFloat(v) * dummyExchangeRate;
-      setRightValue(value);
+    console.log(`the user input ${e.target.value} ${leftCurrency}`);
+    if (v !== "") {
+      const value = parseFloat(v) * DUMMY_EXCHANGE_RATE;
+      setRightValue(value.toFixed(5)); // show 5 decimals ? ?? ?
+      console.log(
+        `the right value is calculated to be: ${value} ${rightCurrency}`
+      );
+    } else {
+      setRightValue(""); // clear right box when left is empty
+    }
+  };
+
+  //FUNCTION TO HANDLE HOW TYPING ON THE *RIGHT* UPDATES THE TEXTBOX ON THE LEFT
+  const onRightChange = (e) => {
+    const v = e.target.value; // let v be what the user typed
+    setRightValue(v);
+    console.log(`the user input ${e.target.value} ${rightCurrency}`);
+    if (v !== "") {
+      const value = parseFloat(v) * DUMMY_EXCHANGE_RATE;
+      setLeftValue(value.toFixed(5)); // show 5 decimals ? ?? ?
+      console.log(
+        `the left value is calculated to be: ${value} ${rightCurrency}`
+      );
+    } else {
+      setLeftValue(""); // clear right box when left is empty
     }
   };
 
@@ -38,7 +58,7 @@ const Converter = () => {
             id="currency"
             value={leftCurrency}
             onChange={(e) => {
-              console.log("from currency has changed to:", e.target.value);
+              console.log("from currency has changed to:", e.target.value); //left dropdown
               setLeftCurrency(e.target.value);
             }}
           >
@@ -53,10 +73,7 @@ const Converter = () => {
               type="number"
               min="0"
               value={leftValue}
-              onChange={(e) => {
-                console.log("left value input:", e.target.value);
-                setLeftValue(e.target.value);
-              }}
+              onChange={onLeftChange} //left input box
             />
           </div>
 
@@ -73,9 +90,9 @@ const Converter = () => {
             id="currency"
             value={rightCurrency}
             onChange={(e) => {
-              console.log("right currency has changed to:", e.target.value);
-              setLeftCurrency(e.target.value);
-            }}
+              setRightCurrency(e.target.value); //right dropdown
+              console.log("right currency has been set to:", e.target.value);
+            }} //right currency
           >
             <option value="usd">USD</option>
             <option value="eur">EUR</option>
@@ -88,31 +105,15 @@ const Converter = () => {
               type="number"
               min="0"
               value={rightValue}
-              onChange={(e) => {
-                console.log("right value input:", e.target.value);
-                setRightValue(e.target.value);
-              }}
+              onChange={onRightChange} //right input
             />
           </div>
 
           <p>$1USD = 1.291SGD</p>
         </div>
       </div>
-      {/* <label htmlFor="basevalue"></label>
-      <input value={baseValue} onChange={handleChange}></input> */}
     </>
   );
 };
 
 export default Converter;
-
-/*
-
-TODOS:
-- create the two cards both on the left and right 
-    - input field 
-    - currency 
-    - flag emoji 
-- create the exchange button (onClick)
-
-*/
