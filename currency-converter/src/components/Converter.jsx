e508b651c403df6ff1698afe0f0449914579d368
junesutan <juneowns@gmail.com>
2025-10-07 10:47:43 +0800

    somewhat got the dropdown and input to store value, next step is for the boxes to be able to take left value and update right, take right value and update left

--- currency-converter/src/components/Converter.jsx
+++ currency-converter/src/components/Converter.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import { useState } from "react";
+
+const dummyExchangeRate = 0.7742;
 
 const Converter = () => {
-  //   const [baseValue, setBaseValue] = useState("0.00");
+  const [leftCurrency, setLeftCurrency] = useState("sgd");
+  const [rightCurrency, setRightCurrency] = useState("usd");
+
+  const [leftValue, setLeftValue] = useState("1.00");
+  const [rightValue, setRightValue] = useState("1.00");
 
   //   const handleChange = (event) => {
   //     setBaseValue(event.target.value);
   //   };
+
+  //WRITING A FUNCTION TO HANDLE HOW TYPING ON THE LEFT UPDATES THE TEXTBOX ON THE RIGHT
+  const onLeftChange = (e) => {
+    const v = e.target.value;
+    setLeftValue(v);
+    if (rate && v !== "") {
+      const value = parseFloat(v) * dummyExchangeRate;
+      setRightValue(value);
+    }
+  };
 
   return (
     <>
@@ -14,14 +31,71 @@ const Converter = () => {
       <div
         style={{ display: "grid", gridTemplateColumns: "1fr 80px 1fr" }} // creates three columns
       >
+        {/* LEFT CARD */}
         <div style={{ background: "grey" }}>
-          <p>This is the left card!</p>
+          {/* <label htmlFor="currency"></label> */}
+          <select
+            id="currency"
+            value={leftCurrency}
+            onChange={(e) => {
+              console.log("from currency has changed to:", e.target.value);
+              setLeftCurrency(e.target.value);
+            }}
+          >
+            <option value="usd">USD</option>
+            <option value="eur">EUR</option>
+            <option value="sgd">SGD</option>
+          </select>
+
+          {/* LEFT INPUT BOX */}
+          <div>
+            <input
+              type="number"
+              min="0"
+              value={leftValue}
+              onChange={(e) => {
+                console.log("left value input:", e.target.value);
+                setLeftValue(e.target.value);
+              }}
+            />
+          </div>
+
+          <p>S$1 = 0.7742USD</p>
         </div>
 
+        {/* SWAP BUTTON */}
         <div>⇄</div>
 
+        {/* RIGHT CARD */}
         <div style={{ background: "grey" }}>
-          <p>This is the right card!</p>
+          {/* <label htmlFor="currency"></label> */}
+          <select
+            id="currency"
+            value={rightCurrency}
+            onChange={(e) => {
+              console.log("right currency has changed to:", e.target.value);
+              setLeftCurrency(e.target.value);
+            }}
+          >
+            <option value="usd">USD</option>
+            <option value="eur">EUR</option>
+            <option value="sgd">SGD</option>
+          </select>
+
+          {/* RIGHT INPUT BOX */}
+          <div>
+            <input
+              type="number"
+              min="0"
+              value={rightValue}
+              onChange={(e) => {
+                console.log("right value input:", e.target.value);
+                setRightValue(e.target.value);
+              }}
+            />
+          </div>
+
+          <p>$1USD = 1.291SGD</p>
         </div>
       </div>
       {/* <label htmlFor="basevalue"></label>
