30fe49c2e7a5adc393f90cd14fd4b8abfff387c8
junesutan <juneowns@gmail.com>
2025-10-07 09:29:08 +0800

    created left and right cards with grid

--- currency-converter/src/components/Converter.jsx
+++ currency-converter/src/components/Converter.jsx
@@ -1,3 +1,44 @@
-export default function Converter() {
-  return <h2>Converter Text</h2>;
-}
+import React from "react";
+
+const Converter = () => {
+  //   const [baseValue, setBaseValue] = useState("0.00");
+
+  //   const handleChange = (event) => {
+  //     setBaseValue(event.target.value);
+  //   };
+
+  return (
+    <>
+      <h1>Compare Currency Instantly!</h1>
+
+      <div
+        style={{ display: "grid", gridTemplateColumns: "1fr 80px 1fr" }} // creates three columns
+      >
+        <div style={{ background: "grey" }}>
+          <p>This is the left card!</p>
+        </div>
+
+        <div>⇄</div>
+
+        <div style={{ background: "grey" }}>
+          <p>This is the right card!</p>
+        </div>
+      </div>
+      {/* <label htmlFor="basevalue"></label>
+      <input value={baseValue} onChange={handleChange}></input> */}
+    </>
+  );
+};
+
+export default Converter;
+
+/*
+
+TODOS:
+- create the two cards both on the left and right 
+    - input field 
+    - currency 
+    - flag emoji 
+- create the exchange button (onClick)
+
+*/
